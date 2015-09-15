9c92dee9406209e05f6b3169b63e4989ce271f40
JeroZhang <jerozhang@aliyun.com>
2015-09-15 22:39:55 +0800

    增加对List《string》的排序

--- MillitStudyJava/src/collection/CollectionsTest.java
+++ MillitStudyJava/src/collection/CollectionsTest.java
@@ -36,11 +36,49 @@ public class CollectionsTest {
 		}
 	}
 	
+	public void sortTest2(){
+		List<String> stringList = new ArrayList<String>();
+		Random random = new Random();
+		CollectionsTest ct = new CollectionsTest();
+		String temp;
+		System.out.println("------生成List<String>-------");
+		for(int i=0;i<10;i++){
+			do{
+				temp = ct.getAString(random.nextInt(10));
+			}while(stringList.contains(temp));
+			stringList.add(temp);
+		}
+		System.out.println("------生成的List<String>-------");
+		for (String string : stringList) {
+			System.out.println(string);
+		}
+		System.out.println("------排序的List<String>--------");
+		Collections.sort(stringList);
+		for (String string : stringList) {
+			System.out.println(string);
+		}
+	}
+	
+	/*
+	 * 产生给定长度的随机字符串
+	 */
+	public String getAString(int size){
+		String allChars = "0123456789abcdefghijklmnopqrstuvwxyz";
+		StringBuilder newString = new StringBuilder();
+		Random random = new Random();
+		
+		while(size>0){
+			size--;
+			newString.append(allChars.charAt(random.nextInt(36)));
+		}
+		return newString.toString();
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CollectionsTest ct = new CollectionsTest();
 		ct.sortTest1();
+		ct.sortTest2();
 	}
 	
 
